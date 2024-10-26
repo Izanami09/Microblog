e14d69da241f0366ac0741b8e074214b1d374f73
Hoa Tran <hoatran@Izanamis-MacBook-Pro-4.local>
2024-10-26 12:21:48 +0545

    First Commit

---
+++ app/routes.py
@@ -0,0 +1,19 @@
+from app import app
+from flask import render_template
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username' : 'Rohan'}
+    posts = [
+        {
+            'author' : {'username': 'John'},
+            'body' : 'Beautiful Day in Portland'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html',  user = user, posts = posts)
+
