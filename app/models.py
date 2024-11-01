5f38a424254d73e04a4c3f1a4cea3fafa489dc34
Hoa Tran <hoatran@Izanamis-MacBook-Pro-4.local>
2024-11-01 16:30:29 +0545

    Profile Avatar and Edit Profile

--- app/models.py
+++ app/models.py
@@ -6,12 +6,15 @@ from datetime import datetime, timezone
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 from app import login
+from hashlib import md5
 
 class User(UserMixin, db.Model):
     id : so.Mapped[int] = so.mapped_column(primary_key=True)
     username : so.Mapped[str] = so.mapped_column(sa.String(64), index= True, unique= True)
     email : so.Mapped[str] = so.mapped_column(sa.String(120), index=True)
     password_hash : so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
+    about_me : so.Mapped[Optional[str]] = so.mapped_column(sa.String(140))
+    last_seen : so.Mapped[Optional[datetime]] = so.mapped_column(default=lambda: datetime.now(timezone.utc))
 
     posts: so.WriteOnlyMapped['Post'] = so.relationship(back_populates='author')
     def __repr__(self):
@@ -23,6 +26,11 @@ class User(UserMixin, db.Model):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
     
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
+    
+
 
 class Post(db.Model):
     id : so.Mapped[int] = so.mapped_column(primary_key=True)
