1a7f6fab656dbd3dfb962937bf0ddc9598c6944b
Hoa Tran <hoatran@Izanamis-MacBook-Pro-4.local>
2024-11-20 23:12:23 +0545

    Pagination

--- config.py
+++ config.py
@@ -10,3 +10,5 @@ class Config:
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['rohan123.rs8@gmail.com']
+    POSTS_PER_PAGE = 10
+    
